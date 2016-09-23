764127ad9ba4a437a443216a71bd27180aac5d87
tomahawk <tmutunhire@gmail.com>
2016-09-24 00:54:53 +0200

    this RiotJS was extraordinarily useful!

---
+++ smallmedia.js
@@ -0,0 +1,2 @@
+riot.tag2('smallmedia', '<div class="media"> <a href="#" class="pull-left"> <img id="cleanupimg" class="img-responsive img-rounded" src="http://placehold.it/60x60" alt="The Neutron Wars"> </a> <div class="media-body"> <h4 class="media-heading">Amos Moses was a cajun</h4> <p> The default ratio depends on the display density. On a display with density less than 200dpi, the ratio is 1.0. On displays with density between 200 and 300dpi, the ratio is 1.5. </p> </div> </div>', '', '', function(opts) {
+});
